f16a559dfa9ae9d55633c781ab93cdfd126faa4d
yusin <yunisden3@gmail.com>
2024-10-26 18:03:48 +0300

    update the service-worker and manifest file

--- service-worker.js
+++ service-worker.js
@@ -25,8 +25,37 @@ self.addEventListener('fetch', event => {
     event.respondWith(
         caches.match(event.request)
             .then(response => {
-                // Return cached version or fetch new
-                return response || fetch(event.request);
+                // If the resource is in the cache, return it
+                if (response) {
+                    return response;
+                }
+
+                // Otherwise, fetch from network
+                return fetch(event.request)
+                    .then(response => {
+                        // Check if we received a valid response
+                        if (!response || response.status !== 200 || response.type !== 'basic') {
+                            return response;
+                        }
+
+                        // Clone the response as it's a stream and can only be consumed once
+                        const responseToCache = response.clone();
+
+                        caches.open(CACHE_NAME)
+                            .then(cache => {
+                                cache.put(event.request, responseToCache);
+                            });
+
+                        return response;
+                    })
+                    .catch(() => {
+                        // If both cache and network fail, return a custom offline response
+                        if (event.request.mode === 'navigate') {
+                            return caches.match('./index.html');
+                        }
+                        // For other resources, you might want to return a default offline asset
+                        return new Response('Offline content not available');
+                    });
             })
     );
 });
